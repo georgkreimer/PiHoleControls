4ef47d98f2ebe2cbd5ea8af9748b228e2a06ef9c
Georg Kreimer <georg@littleappventures.com>
2025-11-25 16:09:47 +0100

    Initial Commit

---
+++ PiHoleControls/PiHoleControlsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PiHoleControlsApp.swift
+//  PiHoleControls
+//
+//  Created by Georg Kreimer on 11/25/25.
+//
+
+import SwiftUI
+
+@main
+struct PiHoleControlsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PiHoleControlsTests/PiHoleControlsTests.swift
@@ -0,0 +1,17 @@
+//
+//  PiHoleControlsTests.swift
+//  PiHoleControlsTests
+//
+//  Created by Georg Kreimer on 11/25/25.
+//
+
+import Testing
+@testable import PiHoleControls
+
+struct PiHoleControlsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
